04daf1b33f0070a4323894e55ba092c03d941dff
jooooonis <xowns3213@naver.com>
2023-01-16 18:36:18 +0900

    feat : MessageList component

---
+++ components/MessageList.js
@@ -0,0 +1,31 @@
+export default function MessageList({ target, initialState }) {
+  const messageList = document.createElement('div');
+  messageList.className = 'MessageList';
+  target.appendChild(messageList);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    if (!this.state) return;
+    messageList.innerHTML = this.state
+      .map(
+        (message) => `
+      <div class='MessageCard'>
+        <img src="${message.image}" />
+        <div class='MessageCard__Content'>
+          <h1>${message.title}</h1>
+          <div>${message.content}</div>
+        </div>
+      </div>
+    `
+      )
+      .join('');
+  };
+
+  this.render();
+}

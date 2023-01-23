f13c7bb095b51f5f11ecfdb990a1c64cc40a3d75
jooooonis <xowns3213@naver.com>
2023-01-23 14:06:57 +0900

    design : navigate hover

--- components/Navigate.js
+++ components/Navigate.js
@@ -2,10 +2,10 @@ export default function Navigate({ target }) {
   const page = document.createElement('div');
   page.className = 'navigate';
 
-  page.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" width="36" height="36">
+  page.innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" width="36" height="36">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
   </svg>
-  <span>HPNY 2023</span>
+  <h1>HPNY 2023</h1>
   `;
 
   this.render = () => {

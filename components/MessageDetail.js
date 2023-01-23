dd5391bff8d8eed0c456dfba66d2a5c2b53dbaba
jooooonis <xowns3213@naver.com>
2023-01-23 13:51:12 +0900

    design : comment styling

--- components/MessageDetail.js
+++ components/MessageDetail.js
@@ -19,6 +19,7 @@ export default function MessageDetail({ target, initialState }) {
           <p>${convertDate(message.createdAt)}</p>
           <h2>${message.content}</h2>
         </li>
+        <hr />
         <div class='comment-list'>
           ${comments
             .map(

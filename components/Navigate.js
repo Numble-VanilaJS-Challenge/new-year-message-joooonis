2e1a510deefeb8c53b3d6d53e845a335d383bb63
jooooonis <xowns3213@naver.com>
2023-01-23 14:18:01 +0900

    feat : navigator by route

--- components/Navigate.js
+++ components/Navigate.js
@@ -1,12 +1,29 @@
+import { push } from '../libs/router';
+
 export default function Navigate({ target }) {
   const page = document.createElement('div');
   page.className = 'navigate';
 
-  page.innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" width="36" height="36">
+  const { pathname } = location;
+  if (pathname === '/') {
+    page.innerHTML = `<h1 class='navigate-logo'>HPNY 2023</h1>
+  `;
+  } else {
+    page.innerHTML = `<svg class='navigate-back'  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" width="36" height="36">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
   </svg>
-  <h1>HPNY 2023</h1>
+  <h1 class='navigate-logo'>HPNY 2023</h1>
   `;
+    const back = page.querySelector('.navigate-back');
+    back.addEventListener('click', () => {
+      history.back();
+    });
+
+    const logo = page.querySelector('.navigate-logo');
+    logo.addEventListener('click', () => {
+      push('/');
+    });
+  }
 
   this.render = () => {
     target.appendChild(page);
